015784917cc1faadf3dc326725a510e6d4725a7b
kamilstalinski <kamstal95@gmail.com>
2022-07-25 08:53:50 +0200

    upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburger');
+const navbar = document.querySelector('.navbar');
+const navbarItems = document.querySelector('.navbar-items');
+
+hamburger.addEventListener('click', () => {
+    navbarItems.classList.toggle('active');
+})
+
+window.addEventListener('scroll', () => {
+    navbar.classList.toggle('sticky', window.scrollY > 0);
+})
+
