824037e8b20474b7fa568f7450aa57716c040a4a
kamilstalinski <kamstal95@gmail.com>
2022-08-12 21:13:18 +0200

    added svg shapes

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ hamburger.addEventListener('click', () => {
     navbarItems.classList.toggle('active');
 })
 
-window.addEventListener('scroll', () => {
-    navbar.classList.toggle('sticky', window.scrollY > 0);
-})
+// window.addEventListener('scroll', () => {
+//     navbar.classList.toggle('sticky', window.scrollY > 0);
+// })
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = () => {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        navbar.style.top = "0";
+        navbar.style.backgroundColor = '#fff'
+    } else {
+        navbar.style.top = "-100px";
+    }
+
+    if (window.scrollY === 0) {
+        navbar.style.removeProperty('background-color')
+    }
+    prevScrollpos = currentScrollPos;
+}
 
 
