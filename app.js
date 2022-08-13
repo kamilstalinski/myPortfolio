a5bad17c665e7c70b2ae9c891c047f117417919c
kamilstalinski <kamstal95@gmail.com>
2022-08-13 11:24:33 +0200

    update of projects section

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ window.onscroll = () => {
         navbar.style.top = "0";
         navbar.style.backgroundColor = '#fff'
         navbar.style.boxShadow = '0 3px 3px #e9eef2'
+        navbar.style.height = '80px'
     } else {
-        navbar.style.top = "-105px";
+        navbar.style.top = "-85px";
     }
 
     if (window.scrollY === 0) {
         navbar.style.removeProperty('background-color')
         navbar.style.removeProperty('box-shadow')
+        navbar.style.height = '100px'
     }
     prevScrollpos = currentScrollPos;
 }
