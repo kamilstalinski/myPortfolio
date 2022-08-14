4f7771d4b29f577ed1f1a10f688efcbf7101e928
kamilstalinski <kamstal95@gmail.com>
2022-08-14 20:42:17 +0200

    conversion img to webp

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const navbarItems = document.querySelector('.navbar-items');
 hamburger.addEventListener('click', () => {
     navbarItems.classList.toggle('active');
     hamburger.classList.toggle('active');
-    burger.classList.toggle('not-active')
-
 });
 
 
